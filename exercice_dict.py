6940a6d56b1d317d6eeaa1103cb0db3be58c11b6
Noura <Nourakanzoua@gmail.com>
2023-12-25 15:27:09 +0100

    dict_exc

--- exercice_dict.py
+++ exercice_dict.py
@@ -1,3 +1,24 @@
-dict_fr_en = {"mot1":"mot_1","mot2":"mot_2","mot3":"mot_3"}
-dic_en_fr = dict_fr_en.fromkeys()
-print(dic_en_fr)
+# dict_fr_en = {"mot1":"mot_1","mot2":"mot_2","mot3":"mot_3"}
+# dic_en_fr = dict_fr_en.fromkeys()
+# print(dic_en_fr)
+# dic1 = {}
+# a=list(dic1.keys())
+# b = list(dic1.values())
+# fr_color = ["rouge" ,"blue","vert","jaune","noir","blanc" ]
+# en_color = ["red","blue","green","yello","black","white"]
+# dict_fr_en = dict(fr_color,en_color)
+key = []
+value = []
+dict_1 = dict(zip(key,value))
+
+def revers_key_value (dict_1):
+#  for key,value in dict_1 :
+#  dict_1 = dict(zip(key,value))
+    dict_2 = dict(zip(value,key))
+    print(dict_2)
+
+key  = ["rouge" ,"blue","vert","jaune","noir","blanc" ]
+value = ["red","blue","green","yello","black","white"]
+dict_fr_en = dict(zip(key,value))  
+y = revers_key_value(dict_fr_en)
+print(y)    
